8a7048061b95d7b957408d98b8172ca510c3d171
Mast3rSoda <63344754+Mast3rSoda@users.noreply.github.com>
2022-03-25 22:02:59 +0100

    Changed SetPixel to array operations (faster)

--- biometria-3/Algorithm.cs
+++ biometria-3/Algorithm.cs
@@ -14,6 +14,9 @@ namespace biometria_3
         public static Bitmap Bernsen(Bitmap bmp, int range, int limit)
         {
             byte[,] grayImage = ImageTo2DByteArray(bmp);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[] vs = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, vs, 0, vs.Length);
 
             for (int i = 0; i < grayImage.GetLength(0); i++)
             {
@@ -105,20 +108,33 @@ namespace biometria_3
                     var current = (int)grayImage[i, j];
                     if (contrast < limit)
                     {
-                        if (mean >= 128)
-                            bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
-                        else bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
+                        //if (mean >= 128)
+                            //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
+                        //else
+                            //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
 
+                        if (mean >= 128)
+                            vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MaxValue;
+                            //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
+                        else
+                            vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MinValue;
+                            //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
                     }
                     else
                     {
                         if (current >= mean)
-                            bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
-                        else bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
+                            vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MaxValue;
+                            //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
+                        else
+                            vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MinValue;
+                            //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
                     }
                 }
 
             }
+
+            Marshal.Copy(vs, 0, data.Scan0, vs.Length);
+            bmp.UnlockBits(data);
 
             return bmp;
         }
@@ -153,6 +169,9 @@ namespace biometria_3
         {
 
             byte[,] grayImage = ImageTo2DByteArray(bmp);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[] vs = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, vs, 0, vs.Length);
 
             for (int i = 0; i < grayImage.GetLength(0); i++)
             {
@@ -243,18 +262,17 @@ namespace biometria_3
                     double standardDeviation = Math.Sqrt((Math.Pow((double)(grayImage[i, j] - mean), 2) + Math.Pow((double)(min - mean), 2) + Math.Pow((double)(max - mean), 2)) / 2);
                     var current = (int)grayImage[i, j];
                     if (current < mean - k * standardDeviation)
-                    {
-                        bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
-                    }
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MinValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
                     else
-                    {
-                        bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
-                    }
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MaxValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
                 }
 
             }
 
-
+            Marshal.Copy(vs, 0, data.Scan0, vs.Length);
+            bmp.UnlockBits(data);
 
             return bmp;
         }
@@ -263,6 +281,9 @@ namespace biometria_3
         {
 
             byte[,] grayImage = ImageTo2DByteArray(bmp);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[] vs = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, vs, 0, vs.Length);
 
             for (int i = 0; i < grayImage.GetLength(0); i++)
             {
@@ -352,18 +373,17 @@ namespace biometria_3
                     int mean = (max + min) / 2;
                     var current = (int)grayImage[i, j];
                     if (current < mean)
-                    {
-                        bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
-                    }
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MinValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
                     else
-                    {
-                        bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
-                    }
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MaxValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
                 }
 
             }
 
-
+            Marshal.Copy(vs, 0, data.Scan0, vs.Length);
+            bmp.UnlockBits(data);
 
             return bmp;
         }
@@ -372,6 +392,9 @@ namespace biometria_3
         {
 
             byte[,] grayImage = ImageTo2DByteArray(bmp);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[] vs = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, vs, 0, vs.Length);
 
             for (int i = 0; i < grayImage.GetLength(0); i++)
             {
@@ -462,17 +485,18 @@ namespace biometria_3
                     var current = (int)grayImage[i, j];
                     if (current < median)
                     {
-                        bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MinValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
                     }
                     else
-                    {
-                        bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
-                    }
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MaxValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
                 }
 
             }
 
-
+            Marshal.Copy(vs, 0, data.Scan0, vs.Length);
+            bmp.UnlockBits(data);
 
             return bmp;
         }
@@ -482,6 +506,9 @@ namespace biometria_3
             const int R = 8;
 
             byte[,] grayImage = ImageTo2DByteArray(bmp);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[] vs = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, vs, 0, vs.Length);
 
             for (int i = 0; i < grayImage.GetLength(0); i++)
             {
@@ -574,16 +601,17 @@ namespace biometria_3
                     var current = (int)grayImage[i, j];
 
                     if (current > mean * (1 + k * standardDeviation / R - 1))
-                        bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MinValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
                     else
-                    {
-                        bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
-                    }
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MaxValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
                 }
 
             }
 
-
+            Marshal.Copy(vs, 0, data.Scan0, vs.Length);
+            bmp.UnlockBits(data);
 
             return bmp;
         }
@@ -593,6 +621,9 @@ namespace biometria_3
             const double R = 0.5;
 
             byte[,] grayImage = ImageTo2DByteArray(bmp);
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            byte[] vs = new byte[data.Stride * data.Height];
+            Marshal.Copy(data.Scan0, vs, 0, vs.Length);
 
             for (int i = 0; i < grayImage.GetLength(0); i++)
             {
@@ -690,16 +721,17 @@ namespace biometria_3
                     double p = 3;
                     double t = mean * (1 + p * Math.Exp(-q * mean) + k * ((standardDeviation / R) - 1));
                     if (current > t)
-                        bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MinValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(0, 0, 0));
                     else
-                    {
-                        bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
-                    }
+                        vs[i * data.Stride + (j * 3)] = vs[i * data.Stride + (j * 3 + 1)] = vs[i * data.Stride + (j * 3 + 2)] = byte.MaxValue;
+                        //bmp.SetPixel(j, i, Color.FromArgb(255, 255, 255));
                 }
 
             }
 
-
+            Marshal.Copy(vs, 0, data.Scan0, vs.Length);
+            bmp.UnlockBits(data);
 
             return bmp;
         }
